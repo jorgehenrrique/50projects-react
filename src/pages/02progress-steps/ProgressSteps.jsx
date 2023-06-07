87fe11e235fe34f6ba8addbc2a6095d03ed3ab50
Jorge H <jorgehenrrique@live.com>
2023-06-07 18:15:22 -0300

    fix: button back

--- src/pages/02progress-steps/ProgressSteps.jsx
+++ src/pages/02progress-steps/ProgressSteps.jsx
@@ -2,22 +2,19 @@ import './ProgressSteps.css';
 
 export default function ProgressSteps() {
   return (
-    <>
-      <h1>Progress Steps</h1>
-      <div className="container-02">
-        <div className="progress-container">
-          <div className="progress" id="progress"></div>
-          <div className="circle active">1</div>
-          <div className="circle">2</div>
-          <div className="circle">3</div>
-          <div className="circle">4</div>
-        </div>
-
-        <div>
-          <button className="btn" id="prev" disabled>Prev</button>
-          <button className="btn" id="next">Next</button>
-        </div>
+    <div className="container-02">
+      <div className="progress-container">
+        <div className="progress" id="progress"></div>
+        <div className="circle active">1</div>
+        <div className="circle">2</div>
+        <div className="circle">3</div>
+        <div className="circle">4</div>
       </div>
-    </>
+
+      <div>
+        <button className="btn" id="prev" disabled>Prev</button>
+        <button className="btn" id="next">Next</button>
+      </div>
+    </div>
   );
 }
