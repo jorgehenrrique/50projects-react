1cc3292583b9a332bf4f67f7501693da94023d2f
Jorge H <jorgehenrrique@live.com>
2023-06-07 16:47:00 -0300

    finalizado proj 01

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
 
   return (
     <div className="container">
-      <NavLink className='item' to='./pages/01expanding-cards/'>ExpandingCards</NavLink>
-      <NavLink className='item' to='./pages/02progress-steps/'>ProgressSteps</NavLink>
-      <NavLink className='item' to='./pages/03rotating-nav-animation/'>RotatingNavAnimation</NavLink>
-      <NavLink className='item' to='./pages/04hidden-search/'>HiddenSearch</NavLink>
-      <NavLink className='item' to='./pages/05blurry-loading/'>BlurryLoading</NavLink>
-      <NavLink className='item' to='./pages/06scroll-animation/'>ScrollAnimation</NavLink>
-      <NavLink className='item' to='./pages/07split-landing-page/'>SplitLandingPage</NavLink>
-      <NavLink className='item' to='./pages/08form-input-wave/'>FormInputWave</NavLink>
+      <NavLink className='item' to='./pages/01expanding-cards/'>Expanding Cards</NavLink>
+      <NavLink className='item' to='./pages/02progress-steps/'>Progress Steps</NavLink>
+      <NavLink className='item' to='./pages/03rotating-nav-animation/'>Rotating Nav Animation</NavLink>
+      <NavLink className='item' to='./pages/04hidden-search/'>Hidden Search</NavLink>
+      <NavLink className='item' to='./pages/05blurry-loading/'>Blurry Loading</NavLink>
+      <NavLink className='item' to='./pages/06scroll-animation/'>Scroll Animation</NavLink>
+      <NavLink className='item' to='./pages/07split-landing-page/'>Split Landing Page</NavLink>
+      <NavLink className='item' to='./pages/08form-input-wave/'>Form Input Wave</NavLink>
     </div>
   )
 }
