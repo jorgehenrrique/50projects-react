50caf0517fc698b11258c72f8c0d26a9d927b601
Jorge H <jorgehenrrique@live.com>
2023-06-10 18:44:29 -0300

    finalizado proj04

--- src/pages/04hidden-search/HiddenSearch.jsx
+++ src/pages/04hidden-search/HiddenSearch.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './HiddenSearch.css';
 import lupa from './img/lupa.png';
 
@@ -6,13 +6,18 @@ export default function HiddenSearch() {
   document.title = 'Hidden Search';
 
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.querySelector('.imput-search04').focus();
+    }
+  }, [open]);
   const handleOpen = () => setOpen(!open);
 
   return (
     <div className='container-04'>
       <input className={`imput-search04 ${open ? 'open04' : ''}`}
         type="text" placeholder='Buscar...' />
-      {/* <button className='btn-04' */}
       <button className={`btn-04 ${open ? 'open04' : ''}`}
         onClick={handleOpen}><img src={lupa} /></button>
     </div>
