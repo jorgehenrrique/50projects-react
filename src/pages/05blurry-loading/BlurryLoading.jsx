438b1bbb250f03b181114472a743eeded82a1dfc
Jorge H <jorgehenrrique@live.com>
2023-06-13 15:08:34 -0300

    finalizado proj 05

--- src/pages/05blurry-loading/BlurryLoading.jsx
+++ src/pages/05blurry-loading/BlurryLoading.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+import './BlurryLoading.css';
+
 
 export default function BlurryLoading() {
   document.title = 'Blurry Loading';
 
+  const [blur, setBlur] = useState(20);
+  const [count, setCount] = useState(0);
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    if (count < 100) {
+      setTimeout(() => {
+        setBlur(blur - 0.2);
+        setCount(count + 1);
+        setOpacity(opacity - 0.01)
+      }, 25);
+    }
+  }, [count, blur, opacity]);
 
   return (
-    <h1>Blurry Loading</h1>
+    <>
+      <div className="container-05" style={{ filter: `blur(${blur}px)` }}></div>
+      <span className='loading-text05' style={{ filter: `opacity(${opacity})` }}>{count}%</span>
+    </>
   );
 }
