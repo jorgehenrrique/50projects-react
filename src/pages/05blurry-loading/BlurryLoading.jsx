f4a8a3e5173f3e109aa7d8e809b91ff2f9100f48
Jorge H <jorgehenrrique@live.com>
2023-06-13 15:56:21 -0300

    Fix: proj 05

--- src/pages/05blurry-loading/BlurryLoading.jsx
+++ src/pages/05blurry-loading/BlurryLoading.jsx
@@ -7,6 +7,7 @@ export default function BlurryLoading() {
 
   const [blur, setBlur] = useState(20);
   const [count, setCount] = useState(0);
+  const [scale, setScale] = useState(1.5);
   const [opacity, setOpacity] = useState(1);
 
   useEffect(() => {
@@ -15,14 +16,15 @@ export default function BlurryLoading() {
         setBlur(blur - 0.2);
         setCount(count + 1);
         setOpacity(opacity - 0.01)
+        setScale(scale - 0.005)
       }, 25);
     }
-  }, [count, blur, opacity]);
+  }, [count, blur, opacity, scale]);
 
   return (
-    <>
-      <div className="container-05" style={{ filter: `blur(${blur}px)` }}></div>
+    <div className='body-05' style={{ overflow: count < 100 ? 'hidden' : 'auto' }}>
+      <div className="container-05" style={{ filter: `blur(${blur}px)`, transform: `scale(${scale})` }}></div>
       <span className='loading-text05' style={{ filter: `opacity(${opacity})` }}>{count}%</span>
-    </>
+    </div>
   );
 }
