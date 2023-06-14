787756c0e41700247ba8bff08baf325f6a79149e
Jorge H <jorgehenrrique@live.com>
2023-06-14 16:12:58 -0300

    finalizado css proj 06

--- src/pages/06scroll-animation/ScrollAnimation.jsx
+++ src/pages/06scroll-animation/ScrollAnimation.jsx
@@ -1,9 +1,49 @@
+import { useEffect } from 'react';
 import './ScrollAnimation.css';
 
 export default function ScrollAnimation() {
   document.title = 'Scroll Animation';
 
+  useEffect(() => {
+    const checkBoxes = () => {
+      const triggerBottom = window.innerHeight / 5 * 4;
+      const boxes = document.querySelectorAll('.box');
+
+      boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+          box.classList.add('show');
+        } else {
+          box.classList.remove('show');
+        }
+      });
+    };
+
+    window.addEventListener('scroll', checkBoxes);
+    checkBoxes();
+
+    return () => {
+      window.removeEventListener('scroll', checkBoxes);
+    };
+  }, []);
+
   return (
-    <h1>Scroll Animation</h1>
+    <div className="container-06">
+      <h1>Scroll to see the animation</h1>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+      <div className="box"><h2>Content</h2></div>
+    </div>
   );
 }
