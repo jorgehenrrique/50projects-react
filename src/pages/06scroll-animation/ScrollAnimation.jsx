6a9c1b236b58cf47bd02103ad229236ac638f0b9
Jorge H <jorgehenrrique@live.com>
2023-06-14 16:37:48 -0300

    fix: proj 06

--- src/pages/06scroll-animation/ScrollAnimation.jsx
+++ src/pages/06scroll-animation/ScrollAnimation.jsx
@@ -2,26 +2,29 @@ import { useEffect } from 'react';
 import './ScrollAnimation.css';
 
 export default function ScrollAnimation() {
-  document.title = 'Scroll Animation';
+  useEffect(() => {
+    document.title = 'Scroll Animation';
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
       const triggerBottom = window.innerHeight / 5 * 4;
-      const boxes = Array.from(document.querySelectorAll('.box'));
+      const boxes = Array.from(document.querySelectorAll('.box-06'));
 
       boxes.forEach((box) => {
         const boxTop = box.getBoundingClientRect().top;
 
         if (boxTop < triggerBottom) {
-          box.classList.add('show');
+          box.classList.add('show-06');
         } else {
-          box.classList.remove('show');
+          box.classList.remove('show-06');
         }
       });
     };
 
     window.addEventListener('scroll', handleScroll);
     handleScroll();
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -30,19 +33,19 @@ export default function ScrollAnimation() {
   return (
     <div className="container-06">
       <h1>Role para ver a animação</h1>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
-      <div className="box"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
+      <div className="box-06"><h2>Content</h2></div>
     </div>
   );
 }
