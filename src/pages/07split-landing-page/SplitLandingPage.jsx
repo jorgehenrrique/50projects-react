c3acbf52f8ccdb4f9c9f8d3f1267e977a9ec659b
Jorge H <jorgehenrrique@live.com>
2023-06-14 16:47:01 -0300

    add title as paginas

--- src/pages/07split-landing-page/SplitLandingPage.jsx
+++ src/pages/07split-landing-page/SplitLandingPage.jsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
 
 export default function SplitLandingPage() {
+  useEffect(() => {
+    document.title = 'Split Landing Page';
+  }, []);
   return (
     <h1>Split Landing Page</h1>
   );
--- src/pages/08form-input-wave/FormInputWave.jsx
+++ src/pages/08form-input-wave/FormInputWave.jsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
 
 export default function FormInputWave() {
+  useEffect(() => {
+    document.title = 'Form Input Wave';
+  }, []);
   return (
     <h1>Form Input Wave</h1>
   );
