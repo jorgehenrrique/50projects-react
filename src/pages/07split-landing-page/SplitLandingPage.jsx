b6ee264b6c13f90777323c1631c4c44e047ee159
Jorge H <jorgehenrrique@live.com>
2023-06-15 18:34:55 -0300

    finalizado css ao proj 07

--- src/pages/07split-landing-page/SplitLandingPage.jsx
+++ src/pages/07split-landing-page/SplitLandingPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './SplitLandingPage.css';
 
 export default function SplitLandingPage() {
@@ -6,18 +6,41 @@ export default function SplitLandingPage() {
     document.title = 'Split Landing Page';
   }, []);
 
+  const [isLeftHovered, setIsLeftHovered] = useState(false);
+  const [isRightHovered, setIsRightHovered] = useState(false);
+
+  const handleLeftMouseEnter = () => {
+    setIsLeftHovered(true);
+  };
+
+  const handleLeftMouseLeave = () => {
+    setIsLeftHovered(false);
+  };
+
+  const handleRightMouseEnter = () => {
+    setIsRightHovered(true);
+  };
+
+  const handleRightMouseLeave = () => {
+    setIsRightHovered(false);
+  };
 
   return (
-    <div className='container-07'>
-      <section className='split07 left07'>
+    <div className={`container-07 ${isLeftHovered ? 'hover-left07' : ''} ${isRightHovered ? 'hover-right07' : ''}`}>
+      <section className='split07 left07'
+        onMouseEnter={handleLeftMouseEnter}
+        onMouseLeave={handleLeftMouseLeave}>
         <h1>Playstation</h1>
         <button className='btn07'>Buy Now</button>
       </section>
 
-      <section className='split07 right07'>
+      <section className='split07 right07'
+        onMouseEnter={handleRightMouseEnter}
+        onMouseLeave={handleRightMouseLeave}>
         <h1>XBox Series</h1>
         <button className='btn07'>Buy Now</button>
       </section>
     </div>
   );
 }
+// className={`panel ${panel.active ? "active" : ""}`}
