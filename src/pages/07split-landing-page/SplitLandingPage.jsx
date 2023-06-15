ad9535606929acfc8aac0b5799661b033412eb3e
Jorge H <jorgehenrrique@live.com>
2023-06-15 17:44:26 -0300

    add css ao proj 07

--- src/pages/07split-landing-page/SplitLandingPage.jsx
+++ src/pages/07split-landing-page/SplitLandingPage.jsx
@@ -5,7 +5,19 @@ export default function SplitLandingPage() {
   useEffect(() => {
     document.title = 'Split Landing Page';
   }, []);
+
+
   return (
-    <h1>Split Landing Page</h1>
+    <div className='container-07'>
+      <section className='split07 left07'>
+        <h1>Playstation</h1>
+        <button className='btn07'>Buy Now</button>
+      </section>
+
+      <section className='split07 right07'>
+        <h1>XBox Series</h1>
+        <button className='btn07'>Buy Now</button>
+      </section>
+    </div>
   );
 }
