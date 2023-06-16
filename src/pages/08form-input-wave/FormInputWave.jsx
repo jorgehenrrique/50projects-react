a2e678eca973fe75c243dcfb5a21c0e384a24ba0
Jorge H <jorgehenrrique@live.com>
2023-06-16 17:53:23 -0300

    add css proj 08

--- src/pages/08form-input-wave/FormInputWave.jsx
+++ src/pages/08form-input-wave/FormInputWave.jsx
@@ -1,10 +1,30 @@
 import { useEffect } from 'react';
+import './FormInputWave.css';
 
 export default function FormInputWave() {
   useEffect(() => {
     document.title = 'Form Input Wave';
   }, []);
+
+
   return (
-    <h1>Form Input Wave</h1>
+    <div className="container-08">
+      <form className="form08">
+        <h1>Login</h1>
+        <div className="form-control08">
+          <input type="text" required />
+          <label>Email</label>
+        </div>
+
+        <div className="form-control08">
+          <input type="password" required />
+          <label>Password</label>
+        </div>
+
+        <button className='btn08'>Login</button>
+        <p className="text08">Não tem uma conta? <a href="#">Registrar</a></p>
+      </form>
+
+    </div>
   );
 }
