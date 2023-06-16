2381cb20dac1f1120da753798e18a7f2eaa777e8
Jorge H <jorgehenrrique@live.com>
2023-06-16 18:44:17 -0300

    finalizada funcao proj 08

--- src/pages/08form-input-wave/FormInputWave.jsx
+++ src/pages/08form-input-wave/FormInputWave.jsx
@@ -1,9 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './FormInputWave.css';
 
 export default function FormInputWave() {
   useEffect(() => {
     document.title = 'Form Input Wave';
+  }, []);
+
+  const labelsRef = useRef([]);
+  useEffect(() => {
+    labelsRef.current.forEach((label) => {
+      const letters = label.textContent.split('');
+
+      const updatedContent = letters.map((letter, idx) => {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        span.style.transitionDelay = `${idx * 50}ms`;
+        return span;
+      });
+
+      label.textContent = '';
+      updatedContent.forEach((letter) => label.appendChild(letter));
+    });
   }, []);
 
 
@@ -13,12 +30,12 @@ export default function FormInputWave() {
         <h1>Login</h1>
         <div className="form-control08">
           <input type="text" required />
-          <label>Email</label>
+          <label ref={(el) => (labelsRef.current[0] = el)}>Email</label>
         </div>
 
         <div className="form-control08">
           <input type="password" required />
-          <label>Password</label>
+          <label ref={(el) => (labelsRef.current[1] = el)}>Password</label>
         </div>
 
         <button className='btn08'>Login</button>
