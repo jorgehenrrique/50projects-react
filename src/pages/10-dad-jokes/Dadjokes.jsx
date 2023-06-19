b0bd732aa0a054afd56980648be1f78f745f78f5
Jorge H <jorgehenrrique@live.com>
2023-06-19 16:51:02 -0300

    finalizado proj 10

--- src/pages/10-dad-jokes/Dadjokes.jsx
+++ src/pages/10-dad-jokes/Dadjokes.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Dadjokes.css';
 
 export default function Dadjokes() {
@@ -6,9 +6,42 @@ export default function Dadjokes() {
     document.title = 'Dad jokes';
   }, []);
 
+  const [joke, setJoke] = useState(null);
+
+  function generateJoke() {
+    fetch('https://icanhazdadjoke.com', {
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then((response) => {
+      if (response.ok && response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Servidor indisponível');
+      }
+    }).then((response) => {
+      setJoke(response.joke);
+    }).catch((error) => {
+      setJoke(error.message);
+    });
+  }
+
+  useEffect(() => {
+    setJoke('Clique em: Gerar outra piada.');
+    generateJoke();
+
+    return (() => {
+      setJoke(null);
+    });
+  }, []);
+
   return (
-    <div className="container-10">
-      <h1>Dad jokes</h1>
+    <div className="body-10">
+      <div className="container-10">
+        <h1>Desafio não ria</h1>
+        <strong>{joke}</strong>
+        <button onClick={generateJoke}>Gerar outra piada</button>
+      </div>
     </div >
   );
 }
