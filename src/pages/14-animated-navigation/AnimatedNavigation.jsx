17cd70dcfb72f3c64ba675668b69ef034cb0f5ef
Jorge H <jorgehenrrique@live.com>
2023-06-23 19:38:26 -0300

    add page do proj 14

---
+++ src/pages/14-animated-navigation/AnimatedNavigation.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+export default function AnimatedNavigation() {
+  useEffect(() => {
+    document.title = 'Animated Navigation';
+  }, []);
+
+
+  return (
+    <div className="body-14">
+
+    </div>
+  );
+}
