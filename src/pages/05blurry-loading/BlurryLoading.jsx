26c6f4dd2a5624318bbc8e3390dfb35b24de1a15
Jorge H <jorgehenrrique@live.com>
2023-06-23 19:02:35 -0300

    fix: proj 05

--- src/pages/05blurry-loading/BlurryLoading.jsx
+++ src/pages/05blurry-loading/BlurryLoading.jsx
@@ -6,26 +6,50 @@ export default function BlurryLoading() {
     document.title = 'Blurry Loading';
   }, []);
 
-  const [blur, setBlur] = useState(20);
-  const [count, setCount] = useState(0);
-  const [scale, setScale] = useState(1.5);
-  const [opacity, setOpacity] = useState(1);
+  // const [blur, setBlur] = useState(20);
+  // const [count, setCount] = useState(0);
+  // const [scale, setScale] = useState(1.5);
+  // const [opacity, setOpacity] = useState(1);
+  const [state, setState] = useState({
+    blur: 20,
+    count: 0,
+    opacity: 1
+  });
 
+  // useEffect(() => {
+  //   if (count < 100) {
+  //     setTimeout(() => {
+  //       setBlur(blur - 0.2);
+  //       setCount(count + 1);
+  //       setOpacity(opacity - 0.01)
+  //       setScale(scale - 0.005)
+  //     }, 25);
+  //   }
+  // }, [count, blur, opacity, scale]);
   useEffect(() => {
-    if (count < 100) {
+    if (state.count < 100) {
       setTimeout(() => {
-        setBlur(blur - 0.2);
-        setCount(count + 1);
-        setOpacity(opacity - 0.01)
-        setScale(scale - 0.005)
+        setState((prev) => ({
+          ...prev,
+          blur: prev.blur - 0.2,
+          count: prev.count + 1,
+          opacity: prev.opacity - 0.01,
+        }));
       }, 25);
     }
-  }, [count, blur, opacity, scale]);
+  }, [state]);
 
   return (
-    <div style={{ overflow: count < 100 ? 'hidden' : 'auto' }}>
-      <div className="container-05" style={{ filter: `blur(${blur}px)`, transform: `scale(${scale})` }}></div>
-      <span className='loading-text05' style={{ filter: `opacity(${opacity})` }}>{count}%</span>
+    // <div style={{ overflow: 'auto' }}>
+    //   <div className="container-05" style={{ filter: `blur(${blur}px)`, transform: `scale(${scale})` }}></div>
+    //   <span className='loading-text05' style={{ filter: `opacity(${opacity})` }}>{count}%</span>
+    // </div>
+
+    <div style={{ overflow: 'hidden' }}>
+      <div className="container-05" style={{ filter: `blur(${state.blur}px)` }} />
+      <span className='loading-text05' style={{ filter: `opacity(${state.opacity})` }}>
+        {state.count}%
+      </span>
     </div>
   );
 }
