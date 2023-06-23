3e1bb6fc01737ac2ed5105366bbc78f4fed53365
Jorge H <jorgehenrrique@live.com>
2023-06-23 13:49:56 -0300

    add page, proj 13

---
+++ src/pages/13-random-choice-picker/RandomChoicePicker.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import './RandomChoicePicker.css';
+
+export default function FAQcollapse() {
+  useEffect(() => {
+    document.title = 'Random choice picker';
+  }, []);
+
+  return (
+    <div className="body-13">
+      <div className="container-13">
+
+      </div >
+    </div>
+  );
+}
