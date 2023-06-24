7ea9988b3dc62094fe5f0d8ce1a8da18b87d8f37
Jorge H <jorgehenrrique@live.com>
2023-06-24 16:21:37 -0300

    montado estrutura proj 14

--- src/pages/14-animated-navigation/AnimatedNavigation.jsx
+++ src/pages/14-animated-navigation/AnimatedNavigation.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import './AnimatedNavigation.css';
 
 export default function AnimatedNavigation() {
   useEffect(() => {
     document.title = 'Animated Navigation';
   }, []);
 
+  const [active, setActive] = useState(false);
 
   return (
     <div className="body-14">
-      <h1>14-animated-navigation</h1>
+      <nav className={`nav-14 ${active ? 'active' : ''}`}>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Works</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <button className="icon toggle" onClick={() => setActive(!active)}>
+          <div className="line line1">+</div>
+          <div className="line line2">+</div>
+        </button>
+      </nav>
     </div>
   );
 }
