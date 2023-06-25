44f08d17be18b7803efc602a18bbf385034fa6b8
Jorge H <jorgehenrrique@live.com>
2023-06-25 17:37:37 -0300

    Feito css proj 15

--- src/pages/15-incrementing-counter/IncrementingCounter.jsx
+++ src/pages/15-incrementing-counter/IncrementingCounter.jsx
@@ -1,3 +1,4 @@
+import './IncrementingCounter.css';
 import { useEffect } from 'react';
 
 export default function IncrementingCounter() {
@@ -7,7 +8,25 @@ export default function IncrementingCounter() {
 
   return (
     <div className="body-15">
-      <h1>Incrementing Counter</h1>
+
+      <div className="counter-container-15">
+        <i className="fab fa-twitter fa-3x"></i>
+        <div className="counter-15" data-target="12000"></div>
+        <span>Twitter Followers</span>
+      </div>
+
+      <div className="counter-container-15">
+        <i className="fab fa-youtube fa-3x"></i>
+        <div className="counter-15" data-target="5000">54</div>
+        <span>YouTube Subscribers</span>
+      </div>
+
+      <div className="counter-container-15">
+        <i className="fab fa-facebook fa-3x"></i>
+        <div className="counter-15" data-target="7500"></div>
+        <span>Instagram Fans</span>
+      </div>
+
     </div>
   );
 }
