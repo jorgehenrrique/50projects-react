769c2ca44257da6adc68ba7246dd42fd8fd5845e
Jorge H <jorgehenrrique@live.com>
2023-07-14 20:26:02 -0300

    proj 17 finalizado

--- src/pages/17-movie-app/MovieApp.jsx
+++ src/pages/17-movie-app/MovieApp.jsx
@@ -1,5 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './MovieApp.css';
+
+function getMovies(url) {
+  return fetch(url)
+    .then(res => res.json())
+    .then(res => res.results)
+}
 
 export default function MovieApp() {
   useEffect(() => {
@@ -7,22 +13,42 @@ export default function MovieApp() {
   }, []);
 
   const [data, setData] = useState();
+  const search = useRef();
 
   const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
   const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
   const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
   useEffect(() => {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(res => setData(res.results))
+    fetchData(API_URL);
   }, []);
-  console.log(data)
+
+  const fetchData = async (url) => {
+    try {
+      const movies = await getMovies(url);
+      setData(movies);
+    } catch {
+      alert('Erro ao obter os filmes.');
+    }
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      const inputSearch = search.current.value.trim();
+      fetchData(SEARCH_API + inputSearch);
+      search.current.value = '';
+    }
+  };
+
+  const home = () => {
+    fetchData(API_URL);
+  };
 
   return (
     <div className="body-17">
       <header className='header-17'>
-        <input type="search" placeholder='Buscar' />
+        <button onClick={home}>Home</button>
+        <input type="search" placeholder='Buscar' ref={search} onKeyUpCapture={handleSearch} />
       </header>
 
       <main className='main-17'>
@@ -37,14 +63,12 @@ export default function MovieApp() {
                   ${movie.vote_average >= 8 ? 'green-17' : ''}`}
                 >{movie.vote_average}</span>
               </div>
-              <div className="visao-geral-17">
-                <h3>Visão geral</h3>
+              <div className="overview-17">
+                {movie.overview && <h3>Overview</h3>}
                 {movie.overview}
               </div>
-            </div>
-          )
+            </div>)
         })}
-
       </main>
     </div >
   );
