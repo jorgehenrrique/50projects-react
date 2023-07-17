35a0f24f836aa2078c41714cc1a59ad3d67b2159
Jorge H <jorgehenrrique@live.com>
2023-07-16 21:48:02 -0300

    otimizado jsx proj 18

--- src/pages/18-background-slider/BackgroundSlider.jsx
+++ src/pages/18-background-slider/BackgroundSlider.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import './BackgroundSlider.css';
 
@@ -15,40 +16,49 @@ export default function BackgroundSlider() {
       'https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80',
       'https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80']
 
-  const handlePositionLeft = () => {
-    if (position === 0) {
-      console.log('tamanho minimo')
-      setPosition(imgs.length - 1)
-    } else {
-      setPosition(position - 1)
+  useEffect(() => {
+    // Pré-carregar as imagens em segundo plano
+    imgs.forEach((imgUrl) => {
+      const link = document.createElement('link');
+      link.rel = 'prefetch';
+      link.href = imgUrl;
+      document.head.appendChild(link);
+    });
 
-    }
+    // Limpeza ao desmontar o componente
+    return () => {
+      // Remover os elementos <link> do cabeçalho do documento
+      imgs.forEach((imgUrl) => {
+        const link = document.querySelector(`link[href="${imgUrl}"][rel="prefetch"]`);
+        if (link) {
+          document.head.removeChild(link);
+        }
+      });
+    };
+  }, []);
+
+  const handlePositionLeft = () => {
+    if (position === 0) setPosition(imgs.length - 1)
+    else setPosition(position - 1)
   };
 
   const handlePositionRight = () => {
-    if (position === imgs.length - 1) {
-      console.log('tamanho maximo')
-      setPosition(0)
-    } else {
-      setPosition(position + 1)
-    }
+    if (position === imgs.length - 1) setPosition(0)
+    else setPosition(position + 1)
   };
 
   return (
     <div className="body-18" style={{ backgroundImage: `url(${imgs[position]})` }}>
       <div className="slider-container-18">
-        {/* {imgs.map((img, i) => {
-          return ( */}
         <div
           className='slide-18'
           style={{ backgroundImage: `url(${imgs[position]})` }}>
         </div>
-        {/* );
-        })} */}
+
         <button className='arrow18 left-arrow-18'
-          onClick={handlePositionLeft}>🔙</button>
+          onClick={handlePositionLeft}>←</button>
         <button className='arrow18 right-arrow-18'
-          onClick={handlePositionRight}>🔜</button>
+          onClick={handlePositionRight}>→</button>
       </div>
     </div >
   );
