04c4905d50cd0d2732c453cf0df8299f2909d581
Jorge H <jorgehenrrique@live.com>
2023-07-16 21:07:28 -0300

    feito jsx proj 18

--- src/pages/18-background-slider/BackgroundSlider.jsx
+++ src/pages/18-background-slider/BackgroundSlider.jsx
@@ -6,7 +6,7 @@ export default function BackgroundSlider() {
     document.title = 'Background Slider';
   }, []);
 
-  const [active, setActive] = useState(true);
+  const [position, setPosition] = useState(0);
 
   const imgs =
     ['https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
@@ -15,19 +15,40 @@ export default function BackgroundSlider() {
       'https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80',
       'https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80']
 
+  const handlePositionLeft = () => {
+    if (position === 0) {
+      console.log('tamanho minimo')
+      setPosition(imgs.length - 1)
+    } else {
+      setPosition(position - 1)
+
+    }
+  };
+
+  const handlePositionRight = () => {
+    if (position === imgs.length - 1) {
+      console.log('tamanho maximo')
+      setPosition(0)
+    } else {
+      setPosition(position + 1)
+    }
+  };
+
   return (
     <div className="body-18">
       <div className="slider-container-18">
         {/* {imgs.map((img, i) => {
           return ( */}
         <div
-          className={`slide-18 ${active ? 'active18' : ''}`}
-          style={{ backgroundImage: `url(${imgs[1]})` }}>
+          className='slide-18'
+          style={{ backgroundImage: `url(${imgs[position]})` }}>
         </div>
         {/* );
         })} */}
-        <button className='arrow18 left-arrow-18'>🔙</button>
-        <button className='arrow18 right-arrow-18'>🔜</button>
+        <button className='arrow18 left-arrow-18'
+          onClick={handlePositionLeft}>🔙</button>
+        <button className='arrow18 right-arrow-18'
+          onClick={handlePositionRight}>🔜</button>
       </div>
     </div >
   );
