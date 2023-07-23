6e0426a9575e5a0206bbbe2b7185aa44c8304ebb
Jorge H <jorgehenrrique@live.com>
2023-07-23 14:42:21 -0300

    Add proj diretorio proj 19

---
+++ src/pages/19-theme-clock/ThemeClock.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react';
+import './ThemeClock.css';
+
+export default function ThemeClock() {
+  useEffect(() => {
+    document.title = 'Theme Clock';
+  }, []);
+
+  return (
+    <div className="body-18">
+      <h1>Olá</h1>
+    </div >
+  );
+}
