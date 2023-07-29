cb2eb1ec30bf33bba3da706ed6eaf02bedcab021
Jorge H <jorgehenrrique@live.com>
2023-07-29 16:04:47 -0300

    proj 20  finalizado

--- src/pages/20-button-ripple-effect/ButtonRippleEffect.jsx
+++ src/pages/20-button-ripple-effect/ButtonRippleEffect.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './ButtonRippleEffect.css';
 
 export default function ButtonRippleEffect() {
@@ -6,10 +6,40 @@ export default function ButtonRippleEffect() {
     document.title = 'Button Ripple Effect';
   }, []);
 
+  const [circle, setCircle] = useState(false);
+  const [style, setStyle] = useState({});
+
+  const handleClick = (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+
+    const buttonLeft = e.target.offsetLeft;
+    const buttonTop = e.target.offsetTop;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    setCircle(true);
+    setTimeout(() => {
+      setCircle(false);
+    }, 500);
+
+    // Criar objeto com as propriedades CSS dinamicamente
+    const circleStyle = {
+      top: yInside + 'px',
+      left: xInside + 'px',
+    };
+
+    // Atualizar o estado com o novo estilo
+    setStyle(circleStyle);
+  };
+
   return (
     <div className='body-20'>
 
-      <button>Click Me</button>
+      <button onClick={handleClick}>Click Me
+        {circle && <span className='circle-20'
+          style={style}>🤡</span>}</button>
 
     </div>
   );
