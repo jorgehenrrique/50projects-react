2187556cc09af312708521f5421336deab0caec9
Jorge H <jorgehenrrique@live.com>
2023-07-30 16:01:55 -0300

    Fix: proj 21

--- src/pages/21-drag-n-drop/DragNDrop.jsx
+++ src/pages/21-drag-n-drop/DragNDrop.jsx
@@ -6,16 +6,18 @@ export default function DragNDrop() {
     document.title = 'Drag N Drop';
   }, []);
 
-  const [isDragging, setIsDragging] = useState(false);
   const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [hold, setHold] = useState(false);
+  const [invisible, setInvisible] = useState(false);
 
   const handleDragStart = () => {
-    setIsDragging(true);
+    setHold(true);
+    setTimeout(() => setInvisible(true), 0);
   };
 
   const handleDragEnd = () => {
-    setIsDragging(false);
-    setHoveredIndex(-1);
+    setHold(false);
+    setInvisible(false);
   };
 
   const handleDragOver = (e) => {
@@ -38,44 +40,44 @@ export default function DragNDrop() {
   return (
     <div className='body-21'>
       <div
-        className={`empty-21${isDragging && hoveredIndex === 0 ? ' hovered-21' : ''}`}
+        className={`empty-21 ${hoveredIndex === 1 && 'hovered-21'}`}
         onDragOver={handleDragOver}
-        onDragEnter={() => handleDragEnter(0)}
+        onDragEnter={() => handleDragEnter(1)}
         onDragLeave={handleDragLeave}
         onDrop={handleDragDrop}
       >
         <div
-          className="fill-21"
+          className={`fill-21 ${hold && 'hold-21'} ${invisible && 'invisible-21'}`}
           draggable={true}
           onDragStart={handleDragStart}
           onDragEnd={handleDragEnd}
         ></div>
       </div>
       <div
-        className={`empty-21${isDragging && hoveredIndex === 1 ? ' hovered-21' : ''}`}
-        onDragOver={handleDragOver}
-        onDragEnter={() => handleDragEnter(1)}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDragDrop}
-      ></div>
-      <div
-        className={`empty-21${isDragging && hoveredIndex === 2 ? ' hovered-21' : ''}`}
+        className={`empty-21 ${hoveredIndex === 2 && 'hovered-21'}`}
         onDragOver={handleDragOver}
         onDragEnter={() => handleDragEnter(2)}
         onDragLeave={handleDragLeave}
         onDrop={handleDragDrop}
       ></div>
       <div
-        className={`empty-21${isDragging && hoveredIndex === 3 ? ' hovered-21' : ''}`}
+        className={`empty-21 ${hoveredIndex === 3 && 'hovered-21'}`}
         onDragOver={handleDragOver}
         onDragEnter={() => handleDragEnter(3)}
         onDragLeave={handleDragLeave}
         onDrop={handleDragDrop}
       ></div>
       <div
-        className={`empty-21${isDragging && hoveredIndex === 4 ? ' hovered-21' : ''}`}
+        className={`empty-21 ${hoveredIndex === 4 && 'hovered-21'}`}
         onDragOver={handleDragOver}
         onDragEnter={() => handleDragEnter(4)}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDragDrop}
+      ></div>
+      <div
+        className={`empty-21 ${hoveredIndex === 5 && 'hovered-21'}`}
+        onDragOver={handleDragOver}
+        onDragEnter={() => handleDragEnter(5)}
         onDragLeave={handleDragLeave}
         onDrop={handleDragDrop}
       ></div>
