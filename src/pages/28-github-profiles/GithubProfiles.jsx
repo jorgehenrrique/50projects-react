0d27055e6472f642c15e5fed14df58b8312c1e46
Jorge H <jorgehenrrique@live.com>
2023-08-13 19:35:42 -0300

    Finalizado search e exibicao de user

--- src/pages/28-github-profiles/GithubProfiles.jsx
+++ src/pages/28-github-profiles/GithubProfiles.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './GithubProfiles.css';
 
 export default function GithubProfiles() {
@@ -6,8 +6,92 @@ export default function GithubProfiles() {
     document.title = 'Github Profiles';
   }, []);
 
+  const APIURL = 'https://api.github.com/users/'
+  const inputSearch = useRef(null);
+  const [githubData, setGithubData] = useState(null);
+  const [githubRepos, setGithubRepos] = useState(null);
+  const [msgError, setMsgError] = useState(null);
+
+  useEffect(() => {
+    return (() => {
+      setGithubData(null);
+      setGithubRepos(null);
+      setMsgError(null);
+    })
+  }, []);
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      getUser(inputSearch.current.value.trim());
+      getRepos(inputSearch.current.value.trim());
+      inputSearch.current.value = '';
+    }
+  };
+
+  const getUser = (username) => {
+    fetch(`${APIURL}${username}`)
+      .then((res) => {
+        if (res.status == 404) {
+          setMsgError(<div className='card-28'><h1>No profile with this username</h1></div>);
+        } else {
+          setMsgError(null);
+          res.json();
+        }
+      })
+      .then((data) => setGithubData(data))
+      .catch((err) => {
+        setMsgError(<div className='card-28'><h1>{err}</h1></div>);
+      })
+  };
+
+  const getRepos = (username) => {
+    fetch(`${APIURL}${username}/repos?sort=created`)
+      .then((res) => res.json())
+      .then((data) => setGithubRepos(data))
+      .catch(() => {
+        setMsgError(<div className='card-28'><h1>Problem fetching repos</h1></div>);
+      })
+  };
+  console.log(githubData)
+  console.log(githubRepos)
+  console.log(msgError)
+
   return (
     <div className='body-28'>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" placeholder='Search a Github User'
+          ref={inputSearch}
+          onKeyUpCapture={handleSearch} />
+      </form>
+
+      <main>
+        {githubData && (
+          <>
+            <div className="card-28">
+              <div>
+                <img src={githubData.avatar_url} alt={githubData.name} className='avatar-28' />
+              </div>
+
+              <div className="user-info-28">
+                <h2>{githubData.name || githubData.login}</h2>
+                {githubData.bio && <p>{githubData.bio}</p>}
+                <ul>
+                  <li>{githubData.fallowers} <strong>Fallowers</strong></li>
+                  <li>{githubData.fallowing} <strong>Fallowing</strong></li>
+                  <li>{githubData.publib_repos} <strong>Repos</strong></li>
+                </ul>
+
+
+                {githubRepos && <div className="repo-28">
+
+                </div>}
+              </div>
+            </div>
+          </>
+        )}
+        {msgError && msgError}
+      </main>
 
     </div>
   );
