090c3d882ad48806ef1a44bb909cd537b4d74655
Jorge H <jorgehenrrique@live.com>
2023-08-22 16:42:04 -0300

    finalizado proj 29

--- src/pages/29-double-click-heart/DoubleClickHeart.jsx
+++ src/pages/29-double-click-heart/DoubleClickHeart.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './DoubleClickHeart.css';
 
 export default function DoubleClickHeart() {
@@ -6,8 +6,56 @@ export default function DoubleClickHeart() {
     document.title = 'Double Click Heart';
   }, []);
 
+  const heart = ['❤️', '💙', '💜', '💚', '🧡', '💛', '🖤', '🤎', '🩶', '🤍', '🩵', '🩷', '❤️‍🔥'];
+
+  const getRandomHeart = () => {
+    const randomIndex = Math.floor(Math.random() * heart.length);
+    return heart[randomIndex];
+  };
+
+  const [like, setLike] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const [style, setStyle] = useState({
+    top: '0px',
+    left: '0px'
+  });
+
+  const handleDoubleClick = (e) => {
+    setLike(like + 1);
+    setClicked(true);
+    createHeart(e);
+
+    setTimeout(() => {
+      setClicked(false);
+    }, 800);
+  };
+
+  const createHeart = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const leftOffset = e.target.offsetLeft;
+    const topOffset = e.target.offsetTop;
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    setStyle({
+      top: `${yInside}px`,
+      left: `${xInside}px`
+    });
+  }
+
   return (
     <div className='body-29'>
+
+      <h3>Double click on the image to <i>♥️</i> it</h3>
+      <small>You liked it <span>{like}</span> times</small>
+
+      <div className="loveMe-29"
+        onDoubleClick={handleDoubleClick}>
+        {clicked && <i className='heart-29' style={style}>{getRandomHeart()}</i>}
+      </div>
 
     </div>
   );
