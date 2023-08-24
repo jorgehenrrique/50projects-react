b29a24bcd837fb2c4afef0205c7b4f7fb8dfe9b3
Jorge H <jorgehenrrique@live.com>
2023-08-24 16:51:13 -0300

    Add jsx

--- src/pages/30-auto-text-effect/AutoTextEffect.jsx
+++ src/pages/30-auto-text-effect/AutoTextEffect.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import './AutoTextEffect.css';
 
 export default function AutoTextEffect() {
@@ -6,21 +6,43 @@ export default function AutoTextEffect() {
     document.title = 'Auto Text Effect';
   }, []);
 
-  const speed = useRef(1);
+  const [textStart, setTextStart] = useState('');
+  const [speed, setSpeed] = useState(1);
+  const text = 'We Love Programming!';
+  const speedText = 300 / speed;
+  let idx = 0;
+  let timeout;
 
-  const handleSpeed = () => {
-    console.log(speed.current.value);
-
+  const handleSpeed = (e) => {
+    const newSpeed = parseInt(e.target.value);
+    setSpeed(newSpeed);
   };
+
+  const writeText = () => {
+    setTextStart(text.slice(0, idx));
+    idx++;
+
+    if (idx > text.length) {
+      idx = 0;
+    }
+
+    timeout = setTimeout(writeText, speedText);
+  };
+
+  useEffect(() => {
+    clearTimeout(timeout); // Limpa qualquer timeout pendente
+    writeText();
+    return () => clearTimeout(timeout); // Limpa o timeout ao desmontar o componente
+  }, [speedText]);
 
   return (
     <div className='body-30'>
 
-      <h1>Starting...</h1>
+      <h1>{textStart || 'Starting...'}</h1>
 
       <div>
         <label htmlFor="speed">Speed:</label>
-        <input type="number" name="speed" defaultValue="1" min="1" max="10" step="1" ref={speed} onChange={handleSpeed} />
+        <input type="number" name="speed" value={speed} min="1" max="10" step="1" onChange={handleSpeed} />
       </div>
 
     </div>
