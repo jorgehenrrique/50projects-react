19cc1e24ea375642ca2e3b6e77c1f8e85f2c5e82
Jorge H <jorgehenrrique@live.com>
2023-08-27 12:05:16 -0300

    Iniciado proj 32

---
+++ src/pages/32-good-cheap-fast/GoodCheapFast.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import './GoodCheapFast.css';
+
+export default function GoodCheapFast() {
+  useEffect(() => {
+    document.title = 'Good Cheap Fast';
+  }, []);
+
+  return (
+    <div className='body-32'>
+
+    </div>);
+}
