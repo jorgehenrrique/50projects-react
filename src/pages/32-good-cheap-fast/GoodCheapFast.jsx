22ddc025c02c2ddab6b5c951f75ad8697a3be51c
Jorge H <jorgehenrrique@live.com>
2023-08-27 12:30:59 -0300

    Add css proj 32

--- src/pages/32-good-cheap-fast/GoodCheapFast.jsx
+++ src/pages/32-good-cheap-fast/GoodCheapFast.jsx
@@ -9,5 +9,30 @@ export default function GoodCheapFast() {
   return (
     <div className='body-32'>
 
+      <h2>How do you want your project to be?</h2>
+      <div className="toggle-container-32">
+        <input type="checkbox" id="good" className="toggle-32" />
+        <label htmlFor="good">
+          <div className="ball-32"></div>
+        </label>
+        <span>Good</span>
+      </div>
+
+      <div className="toggle-container-32">
+        <input type="checkbox" id="cheap" className="toggle-32" />
+        <label htmlFor="cheap">
+          <div className="ball-32"></div>
+        </label>
+        <span>Cheap</span>
+      </div>
+
+      <div className="toggle-container-32">
+        <input type="checkbox" id="fast" className="toggle-32" />
+        <label htmlFor="fast">
+          <div className="ball-32"></div>
+        </label>
+        <span>Fast</span>
+      </div>
+
     </div>);
 }
