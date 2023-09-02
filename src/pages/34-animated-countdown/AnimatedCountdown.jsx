cb34d9f9ca4036b7ee8c845190dd863b6afce635
Jorge H <jorgehenrrique@live.com>
2023-09-02 15:29:00 -0300

    Finalizado proj 34

--- src/pages/34-animated-countdown/AnimatedCountdown.jsx
+++ src/pages/34-animated-countdown/AnimatedCountdown.jsx
@@ -6,44 +6,38 @@ export default function AnimatedCountdown() {
     document.title = 'Animated Countdown';
   }, []);
 
-
-  // const count = [3, 2, 1, 0];
-
   const [show, setShow] = useState(false);
-  // const [hide, setHide] = useState(false);
   const [num, setNum] = useState(3);
+  const [classis, setClassis] = useState('');
 
   useEffect(() => {
-
+    setClassis('in');
     setTimeout(() => {
       if (num > 0) setNum(num - 1);
       if (num === 0) setShow(true);
     }, 1000);
-
-    return (() => {
-      // setNum(3);
-      // setShow(false);
-    });
   }, [num]);
 
   console.log(num);
+
+  const handleReplay = () => {
+    setShow(false);
+    setNum(3);
+  };
 
   return (
     <div className='body-34'>
 
       <div className={`counter-34 ${show ? 'hide-34' : ''} `}>
         <div className="nums-34">
-          <span className="in">3</span>
-          <span>2</span>
-          <span>1</span>
-          <span>0</span>
+          <span className={classis} onAnimationEnd={() => setClassis('out')}>{num}</span>
         </div>
         <h4>Get Ready</h4>
       </div>
 
       <div className={`final-34 ${show ? 'show-34' : ''}`}>
         <h1>GO</h1>
-        <button id="replay">
+        <button onClick={handleReplay}>
           <span>Replay</span>
         </button>
       </div>
