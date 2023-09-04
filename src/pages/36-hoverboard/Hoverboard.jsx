d3619802a5df4b73c1e2d21dba4a380f7b35f59b
Jorge H <jorgehenrrique@live.com>
2023-09-04 15:47:00 -0300

    Fix: proj 36

--- src/pages/36-hoverboard/Hoverboard.jsx
+++ src/pages/36-hoverboard/Hoverboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './Hoverboard.css';
 
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71', '#ffff00'];
 const SQUARES = 900;
 
 export default function Hoverboard() {
