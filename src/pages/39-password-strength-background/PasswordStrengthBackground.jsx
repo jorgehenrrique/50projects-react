7ebdb93de8aefa174f1d0e090c9292bd4bb5a602
Jorge H <jorgehenrrique@live.com>
2023-09-05 17:48:40 -0300

    Add css proj 39

--- src/pages/39-password-strength-background/PasswordStrengthBackground.jsx
+++ src/pages/39-password-strength-background/PasswordStrengthBackground.jsx
@@ -9,5 +9,25 @@ export default function PasswordStrengthBackground() {
   return (
     <div className='body-39'>
 
+      <div className="background-39"></div>
+      <div className="form-box-39">
+        <div>
+          <h1>Image Password Strength</h1>
+          <p>Change the password to see the effect</p>
+        </div>
+
+        <div className="inputs-39">
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" placeholder="Enter Email" />
+        </div>
+
+        <div className="inputs-39">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" placeholder="Enter Password" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </div>
+
     </div>);
 }
