bfd085d712e021c77b9d6b48febd0dd14f5943da
Jorge H <jorgehenrrique@live.com>
2023-09-05 18:14:09 -0300

    Finalizado proj 39

--- src/pages/39-password-strength-background/PasswordStrengthBackground.jsx
+++ src/pages/39-password-strength-background/PasswordStrengthBackground.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './PasswordStrengthBackground.css';
 
 export default function PasswordStrengthBackground() {
@@ -6,10 +6,19 @@ export default function PasswordStrengthBackground() {
     document.title = 'Password Strength Background';
   }, []);
 
+  const [blur, setBlur] = useState('20px');
+  const passwordRef = useRef();
+
+  const handlePassword = () => {
+    const length = passwordRef.current.value.trim().length;
+    const blurValue = 20 - length * 2;
+    setBlur(`${blurValue}px`);
+  };
+
   return (
     <div className='body-39'>
 
-      <div className="background-39"></div>
+      <div className="background-39" style={{ filter: `blur(${blur})` }}></div>
       <div className="form-box-39">
         <div>
           <h1>Image Password Strength</h1>
@@ -23,9 +32,9 @@ export default function PasswordStrengthBackground() {
 
         <div className="inputs-39">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="Enter Password" />
+          <input type="password" id="password" ref={passwordRef}
+            placeholder="Enter Password" onChange={handlePassword} />
         </div>
-
         <button type="submit">Submit</button>
       </div>
 
