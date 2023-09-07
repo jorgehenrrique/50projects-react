afff7f3968a10e08dfecf382007e7b574819d672
Jorge H <jorgehenrrique@live.com>
2023-09-07 14:04:34 -0300

    Fix: proj 40

--- src/pages/40-3d-boxes-background/3dBackgroundBoxes.jsx
+++ src/pages/40-3d-boxes-background/3dBackgroundBoxes.jsx
@@ -7,11 +7,11 @@ export default function BackgroundBoxes3D() {
   }, []);
 
   const [classBig, setClassBig] = useState(true);
-  let box = [];
+  let boxes = [];
 
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
-      box.push(<div key={box.length} className="box-40"
+      boxes.push(<div key={boxes.length} className="box-40"
         style={{ backgroundPosition: `${-j * 125}px ${-i * 125}px` }}></div>);
     }
   }
@@ -24,7 +24,7 @@ export default function BackgroundBoxes3D() {
     <div className='body-40'>
 
       <button className="magic-40" onClick={handleBox}>Magic 🎩</button>
-      <div className={`boxes-40 ${classBig ? 'big-40' : ''}`}>{box}</div>
+      <div className={`boxes-40 ${classBig ? 'big-40' : ''}`}>{boxes}</div>
 
     </div>);
 }
