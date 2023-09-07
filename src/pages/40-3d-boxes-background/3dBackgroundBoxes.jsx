9a2465f7e579f62ed31d812694bac43873dfa6da
Jorge H <jorgehenrrique@live.com>
2023-09-07 13:50:06 -0300

    Finalizado proj 40

--- src/pages/40-3d-boxes-background/3dBackgroundBoxes.jsx
+++ src/pages/40-3d-boxes-background/3dBackgroundBoxes.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './3dBackgroundBoxes.css';
 
 export default function BackgroundBoxes3D() {
@@ -6,8 +6,32 @@ export default function BackgroundBoxes3D() {
     document.title = '3D Background Boxes';
   }, []);
 
+  const [classBig, setClassBig] = useState(true);
+  let box = [];
+
+  const handleBox = () => {
+    console.log('oi')
+    setClassBig(!classBig);
+  };
+
+  useEffect(() => {
+    // createBoxes();
+  }, []);
+
+  // const createBoxes = () => {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      box.push(<div className="box-40"
+        style={{ backgroundPosition: `${-j * 125}px ${-i * 125}px` }}></div>);
+    }
+  }
+  // };
+  console.log(box);
   return (
     <div className='body-40'>
+
+      <button className="magic-40" onClick={handleBox}>Magic 🎩</button>
+      <div className={`boxes-40 ${classBig ? 'big-40' : ''}`}>{box}</div>
 
     </div>);
 }
