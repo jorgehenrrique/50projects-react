8399b697aab83f3fe823137d4af82c7dd36dad19
Jorge H <jorgehenrrique@live.com>
2023-09-08 13:26:47 -0300

    Add css proj 41

--- src/pages/41-verify-account-ui/VerifyAccountUi.jsx
+++ src/pages/41-verify-account-ui/VerifyAccountUi.jsx
@@ -9,5 +9,23 @@ export default function VerifyAccountUi() {
   return (
     <div className='body-41'>
 
+      <div className="container-41">
+        <h2>Verify Your Account</h2>
+        <p>We emailed you the six digit code to cool_guy@email.com <br /> Enter the code below to confirm your email address.</p>
+
+        <div className="code-container-41">
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+        </div>
+
+        <small>
+          This is design only. We didn&#39;t actually send you an email as we don&#39;t have your email, right?
+        </small>
+      </div>
+
     </div>);
 }
