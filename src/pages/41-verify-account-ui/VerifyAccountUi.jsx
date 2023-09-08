de6443fb590e1883d1ec5133fdd60ff82b900369
Jorge H <jorgehenrrique@live.com>
2023-09-08 15:53:56 -0300

    Finalizado proj 41

--- src/pages/41-verify-account-ui/VerifyAccountUi.jsx
+++ src/pages/41-verify-account-ui/VerifyAccountUi.jsx
@@ -1,10 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './VerifyAccountUi.css';
 
 export default function VerifyAccountUi() {
   useEffect(() => {
     document.title = 'Verify Account Ui';
   }, []);
+
+  const numbers = [];
+  for (let i = 0; i < 6; i++) {
+    numbers.push(<input
+      key={i}
+      type="number"
+      className="code-41"
+      onKeyDown={(e) => handleNumbers(e, i)}
+      placeholder="0" min="0" max="9" required />)
+  }
+
+  let codes = [];
+  useEffect(() => {
+    codes = document.querySelectorAll('.code-41');
+    codes[0].focus()
+  }, [codes]);
+
+  const handleNumbers = (e, idx) => {
+    if (e.key.trim() >= 0 && e.key.trim() <= 9) {
+      codes[idx].value = '';
+      if (idx === codes.length - 1) return;
+      setTimeout(() => codes[idx + 1].focus(), 10);
+    } else if (e.key === 'Backspace') {
+      if (idx === 0) return;
+      setTimeout(() => codes[idx - 1].focus(), 10);
+    } else if (isNaN(e.key)) {
+      codes[idx].value = '';
+      e.target.value = '';
+      console.log('Entrada não é um número');
+      return;
+    }
+  };
 
   return (
     <div className='body-41'>
@@ -14,12 +46,7 @@ export default function VerifyAccountUi() {
         <p>We emailed you the six digit code to cool_guy@email.com <br /> Enter the code below to confirm your email address.</p>
 
         <div className="code-container-41">
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
-          <input type="number" className="code-41" placeholder="0" min="0" max="9" required />
+          {numbers}
         </div>
 
         <small>
