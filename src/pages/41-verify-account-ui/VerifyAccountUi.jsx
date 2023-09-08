5ae00532261228e5b903483a4740fa439ae07943
Jorge H <jorgehenrrique@live.com>
2023-09-08 16:17:07 -0300

    Fix: proj 41

--- src/pages/41-verify-account-ui/VerifyAccountUi.jsx
+++ src/pages/41-verify-account-ui/VerifyAccountUi.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react';
 import './VerifyAccountUi.css';
 
 export default function VerifyAccountUi() {
@@ -6,34 +7,32 @@ export default function VerifyAccountUi() {
     document.title = 'Verify Account Ui';
   }, []);
 
-  const numbers = [];
+  let codes = [];
+  useEffect(() => {
+    codes = document.querySelectorAll('.code-41');
+    codes[0].focus();
+  }, []);
+
+  const inputs = [];
   for (let i = 0; i < 6; i++) {
-    numbers.push(<input
+    inputs.push(<input
       key={i}
       type="number"
-      className="code-41"
+      className='code-41'
       onKeyDown={(e) => handleNumbers(e, i)}
       placeholder="0" min="0" max="9" required />)
   }
 
-  let codes = [];
-  useEffect(() => {
-    codes = document.querySelectorAll('.code-41');
-    codes[0].focus()
-  }, [codes]);
-
   const handleNumbers = (e, idx) => {
-    if (e.key.trim() >= 0 && e.key.trim() <= 9) {
-      codes[idx].value = '';
+    if (e.key >= 0 && e.key <= 9) {
+      e.target.value = '';
       if (idx === codes.length - 1) return;
       setTimeout(() => codes[idx + 1].focus(), 10);
     } else if (e.key === 'Backspace') {
       if (idx === 0) return;
       setTimeout(() => codes[idx - 1].focus(), 10);
     } else if (isNaN(e.key)) {
-      codes[idx].value = '';
       e.target.value = '';
-      console.log('Entrada não é um número');
       return;
     }
   };
@@ -44,11 +43,9 @@ export default function VerifyAccountUi() {
       <div className="container-41">
         <h2>Verify Your Account</h2>
         <p>We emailed you the six digit code to cool_guy@email.com <br /> Enter the code below to confirm your email address.</p>
-
         <div className="code-container-41">
-          {numbers}
+          {inputs}
         </div>
-
         <small>
           This is design only. We didn&#39;t actually send you an email as we don&#39;t have your email, right?
         </small>
