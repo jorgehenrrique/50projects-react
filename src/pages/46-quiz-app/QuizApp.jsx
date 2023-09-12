764976c0246793085a49bf21f0c49c489025ec9a
Jorge H <jorgehenrrique@live.com>
2023-09-12 11:57:36 -0300

    Add css proj 46

--- src/pages/46-quiz-app/QuizApp.jsx
+++ src/pages/46-quiz-app/QuizApp.jsx
@@ -9,5 +9,33 @@ export default function QuizApp() {
   return (
     <div className='body-46'>
 
+      <div className="quiz-container-46">
+        <div className="quiz-header-46">
+          <h2>Question text</h2>
+          <ul>
+            <li>
+              <input type="radio" name="answer" id="a" className="answer" />
+              <label htmlFor="a" id="a_text">Question</label>
+            </li>
+
+            <li>
+              <input type="radio" name="answer" id="b" className="answer" />
+              <label htmlFor="b" id="b_text">Question</label>
+            </li>
+
+            <li>
+              <input type="radio" name="answer" id="c" className="answer" />
+              <label htmlFor="c" id="c_text">Question</label>
+            </li>
+
+            <li>
+              <input type="radio" name="answer" id="d" className="answer" />
+              <label htmlFor="d" id="d_text">Question</label>
+            </li>
+          </ul>
+        </div>
+        <button>Submit</button>
+      </div>
+
     </div>);
 }
