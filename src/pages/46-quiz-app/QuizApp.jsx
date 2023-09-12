12b0229ebf6de10ba9c249f7fdeb694375c5308e
Jorge H <jorgehenrrique@live.com>
2023-09-12 13:33:00 -0300

    Finalizado proj 46

--- src/pages/46-quiz-app/QuizApp.jsx
+++ src/pages/46-quiz-app/QuizApp.jsx
@@ -1,41 +1,151 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './QuizApp.css';
+
+const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    options: {
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+    },
+    correct: "d",
+  },
+  {
+    question: "What does CSS stand for?",
+    options: {
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cars SUVs Sailboats",
+    },
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    options: {
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopters Terminals Motorboats Lamborginis",
+    },
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    options: {
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+    },
+    correct: "b",
+  },
+];
 
 export default function QuizApp() {
   useEffect(() => {
     document.title = 'Quiz App';
   }, []);
 
+  const [quizState, setQuizState] = useState({
+    idx: 0,
+    corrects: 0,
+    quizFinished: false,
+    selectedAnswer: null,
+  });
+
+  const { idx, corrects, quizFinished, selectedAnswer } = quizState;
+  const { question, options, correct } = quizData[idx];
+
+  const questionRefs = {
+    a: useRef(null),
+    b: useRef(null),
+    c: useRef(null),
+    d: useRef(null),
+  };
+
+  useEffect(() => {
+    if (quizFinished) {
+      return;
+    }
+  }, [idx, quizFinished]);
+
+  function handleQuestions() {
+    if (quizFinished || selectedAnswer === null) {
+      return;
+    }
+
+    if (selectedAnswer === correct) {
+      setQuizState((prevState) => ({
+        ...prevState,
+        corrects: prevState.corrects + 1,
+      }));
+    }
+
+    // Desmarca a opção selecionada
+    questionRefs[selectedAnswer].current.checked = false;
+
+    if (idx < quizData.length - 1) {
+      setQuizState((prevState) => ({
+        ...prevState,
+        idx: prevState.idx + 1,
+        selectedAnswer: null,
+      }));
+    } else {
+      setQuizState((prevState) => ({
+        ...prevState,
+        quizFinished: true,
+      }));
+    }
+  }
+
+  function handleReload() {
+    setQuizState({
+      idx: 0,
+      corrects: 0,
+      quizFinished: false,
+      selectedAnswer: null,
+    });
+  }
+
   return (
     <div className='body-46'>
 
       <div className="quiz-container-46">
         <div className="quiz-header-46">
-          <h2>Question text</h2>
-          <ul>
-            <li>
-              <input type="radio" name="answer" id="a" className="answer" />
-              <label htmlFor="a" id="a_text">Question</label>
-            </li>
-
-            <li>
-              <input type="radio" name="answer" id="b" className="answer" />
-              <label htmlFor="b" id="b_text">Question</label>
-            </li>
-
-            <li>
-              <input type="radio" name="answer" id="c" className="answer" />
-              <label htmlFor="c" id="c_text">Question</label>
-            </li>
-
-            <li>
-              <input type="radio" name="answer" id="d" className="answer" />
-              <label htmlFor="d" id="d_text">Question</label>
-            </li>
-          </ul>
+          {quizFinished ? (
+            <h2>{`You answered ${corrects}/${quizData.length} questions correctly`}</h2>
+          ) : (
+            <>
+              <h2>{question}</h2>
+              <ul>
+                {Object.entries(options).map(([key, value]) => (
+                  <li key={key}>
+                    <input
+                      type="radio"
+                      name="answer"
+                      id={key}
+                      ref={questionRefs[key]}
+                      className="answer"
+                      checked={selectedAnswer === key}
+                      onChange={() => setQuizState((prevState) => ({
+                        ...prevState,
+                        selectedAnswer: key,
+                      }))}
+                    />
+                    <label htmlFor={key} id={`${key}_text`}>{value}</label>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
-        <button>Submit</button>
+        {!quizFinished && (<button onClick={handleQuestions}>Submit</button>)}
+        {quizFinished && (<button onClick={handleReload}>Reload</button>)}
       </div>
 
-    </div>);
+    </div>
+  );
 }
