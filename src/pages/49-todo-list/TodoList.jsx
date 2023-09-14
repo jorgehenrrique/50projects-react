0b5c6b624992c76abc355ca4557c9ef9c4ac1ff7
Jorge H <jorgehenrrique@live.com>
2023-09-14 17:26:15 -0300

    Finalizado proj 49

--- src/pages/49-todo-list/TodoList.jsx
+++ src/pages/49-todo-list/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './TodoList.css';
 
 export default function TodoList() {
@@ -6,8 +6,57 @@ export default function TodoList() {
     document.title = 'Todo List';
   }, []);
 
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) ?? []);
+
+  const handleform = (e) => {
+    e.preventDefault();
+
+    if (input.trim() !== '') {
+      setTodos([...todos, { text: input.trim(), completed: false }]);
+      setInput('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((_, idx) => idx !== index);
+    setTodos(newTodos);
+  };
+
+  const handleToggle = (index) => {
+    const newTodos = todos.map((todo, idx) => {
+      if (idx === index) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className='body-49'>
+
+      <h1>todos</h1>
+      <form onSubmit={handleform}>
+        <input type="text" className="input"
+          placeholder="Enter your todo" autoComplete="off"
+          value={input} onChange={(e) => setInput(e.target.value)} />
+
+        <ul className="todos-49">
+          {todos.map((todo, idx) => (
+            <li key={idx} onContextMenu={(e) => { e.preventDefault(); handleDelete(idx); }}
+              onClick={() => handleToggle(idx)}
+              className={todo.completed ? 'completed-49' : ''}>
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </form>
+      <small>Left click to toggle completed. <br /> Right click to delete todo</small>
 
     </div>);
 }
